fd02e40850508011e814751694589081b5256369
hazem <hazem.foda000@gmail.com>
2023-05-16 00:28:02 +0300

    add status , user controllers

---
+++ Louver/Controllers/StatusController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Louver.Models;
+
+namespace Louver.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StatusController : ControllerBase
+    {
+        private readonly LouverContext _context;
+
+        public StatusController(LouverContext context)
+        {
+            _context = context;
+        }
+
+
+
+        // GET: api/Status/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Status>> GetStatus(int id)
+        {
+            if (_context.Statuses == null)
+            {
+                return NotFound();
+            }
+            var status = await _context.Statuses.FindAsync(id);
+
+            if (status == null)
+            {
+                return NotFound();
+            }
+
+            return status;
+        }
+
+
+
+    }
+       
+}
---
+++ Louver/Controllers/UsersController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Louver.Models;
+using Microsoft.Data.SqlClient;
+
+namespace Louver.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly LouverContext _context;
+
+        public UsersController(LouverContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<User>> GetUser(string userName, string password)
+        {
+            if (_context.Users == null)
+            {
+                return NotFound();
+            }
+            var user = await _context.Users.Where(u => u.UserName == userName && u.Password == password && u.StatusId == 1).ToListAsync();
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(user);
+        }
+        //[HttpGet]
+        //public async Task<ActionResult<User>> GetUser(string userName, string password)
+        //{
+        //    var userNameParam = new SqlParameter("@pUserName", userName);
+        //    var passwordParam = new SqlParameter("@passwordHash", password);
+        //    var statusIDParam = new SqlParameter("@pStatusID ", 1);
+        //    var flagParam = new SqlParameter("@pFlag", 1);
+
+
+
+        //    if (_context.Users == null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    var user = await _context.Users
+        //        .FromSqlRaw("exec GetUsers @pUserName, @passwordHash,@pStatusID,@pFlag", userNameParam, passwordParam, statusIDParam, flagParam)
+        //        .ToListAsync();
+
+        //    if (user == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    return Ok( user);
+        //}
+       
+
+
+       
+    }
+}
